efa89b35ccaeb2a4809eae0ccdbfb4061d981c02
christiandarvs <christiandarvin4@gmail.com>
2024-05-11 17:05:55 +0800

    add files

---
+++ sentiment.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from string import punctuation
+from nltk.stem import PorterStemmer
+from textblob import TextBlob
+
+# nltk.download("all")
+
+
+# pip install -r requirements.txt
+def preprocessing(text):
+    text_token = word_tokenize(text, language="english")
+    stopwords_english = stopwords.words("english")
+    clean_words = []
+    for word in text_token:
+        if word not in stopwords_english and word not in punctuation:
+            clean_words.append(word)
+
+    stemmer = PorterStemmer()
+    stem_words = []
+    for word in clean_words:
+        stem_word = stemmer.stem(word)
+        stem_words.append(stem_word)
+    return " ".join(stem_words)
+
+
+def analyze_sentiment(clean_words):
+    textblob = TextBlob(clean_words)
+    return textblob.sentiment.polarity, clean_words
---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import sentiment
+
+
+# import numpy as np
+# from sklearn.datasets import make_regression
+# from sklearn.linear_model import LinearRegression
+# from sklearn.model_selection import train_test_split
+st.set_page_config(page_title="My Streamlit App")
+remarks = 0
+processed_word = ""
+st.title("Introduction to Sentiment Analysis")
+# sentiment_analyzer.preprocessing("hello world bruh")
+user_input = st.text_area(label="Enter some text", height=150)
+col1, col2 = st.columns(2)
+with col1:
+    if st.button("Analyze", use_container_width=True, type="primary"):
+        clean_word = sentiment.preprocessing(user_input)
+        remarks, processed_word = sentiment.analyze_sentiment(clean_word)
+
+with col2:
+    if st.button("Clear", use_container_width=True, type="secondary"):
+        remarks = 0
+
+
+if remarks > 0:
+    st.header(":green[Positive]")
+elif remarks < 0:
+    st.header(":red[Negative]")
+elif remarks == 0:
+    st.header(":grey[Neutral]")
